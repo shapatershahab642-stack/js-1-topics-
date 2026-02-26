1be16bb425b02d671f1881f52c717834525a08f0
zohaib ali <shapatershahab642@gmail.com>
2026-02-26 23:44:21 +0500

    Add files via upload

---
+++ firsttopic.js
@@ -0,0 +1,111 @@
+// let Name = "zohaib";
+// let fatherName = "ilyasAli";
+// let fullName = Name + " " + fatherName; 
+// alert (fullName)
+
+//  alert("Entter the value  of a!")
+//  let a = prompt("Enter a here","578")
+//  a = Number.parseInt(a)
+//  alert("You entered a of type" + (typeof  a))
+//  document.Write(a)
+
+
+
+
+// alert("Enter the value  of a")
+//  let a = prompt("Enter a here","578")
+//  a = Number.parseInt(a)
+// alert("You entered a of type" +( typeof a))
+//  document.Write(a) 
+
+
+
+
+// let abc = "kuch bhi"
+// console.log(abc)
+  
+
+
+
+// let num = 12
+// let num2 = "12"
+// let result = num + num2  ///1212
+// console.log( typeof result)     /// string ha  ye // 
+
+
+
+
+
+
+
+
+// //declare and in initialize a variable by  name of sum and its value is 12
+//  var sum;
+
+//  // initialize    
+//  sum =  12;
+
+//  console.log(sum)  /// 12
+
+
+ 
+
+
+
+ // Incremem=nt ++
+ // Decrement --
+
+// let a = 1;
+// console.log(++a) /// increment hua ha phla 
+
+// let a = 1;
+// console.log(--a) /// decrement hua  ha ye
+
+// let a = 1;
+// console.log(++a,a) /// increment hua  ha ye  
+
+// let a = 1;
+// console.log(--a;a) /// decremnet hua  ha ye
+
+// let a = 1;
+// let b = 5;
+// console.log(a++ + ++b -a)  //5 => ///is me incremnet or preincrement bhi samil ha
+
+
+
+
+
+
+
+
+
+
+
+let username = "wajhi";
+
+if(username == "wajhi"){
+    console.log("hello wajhi!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if , else if , else //
+
+
